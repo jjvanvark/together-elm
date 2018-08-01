9655c825cf07fa3920936fe5d181ee0f7f75d791
Joost van Vark <joostvanvark@gmail.com>
2018-08-02 00:05:30 +0200

    Initial project init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './main.scss';
+import { Main } from './Main.elm';
+import registerServiceWorker from './registerServiceWorker';
+
+Main.embed(document.getElementById('root'));
+
+registerServiceWorker();
